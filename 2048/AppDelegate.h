2e903a902afdf4d1f5c78eaecfa2bad1c07ae790
飞龙在天 <feilongzaitian@feilongzaitiandeiMac.local>
2014-10-25 19:40:23 +0800

    Initial Commit

---
+++ 2048/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2048
+//
+//  Created by 飞龙在天 on 14-10-25.
+//  Copyright (c) 2014年 飞龙在天. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2048/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2048
+//
+//  Created by 飞龙在天 on 14-10-25.
+//  Copyright (c) 2014年 飞龙在天. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
